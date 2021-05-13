cc1e4b38dbcb7991733d1cc82e0e2d80aa6bc70f
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-13 16:43:48 +0200

    setting up the project
    Testing project

---
+++ CSharpTraining/CSharpTrainingTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CSharpTrainingTest
+{
+	/*
+	 * Now there's one last step that we need to perform 
+	 * in order for this to be a working testing project.
+	 * We need to aasociate these two project together so
+	 * that the test can actually see the classes in our
+	 * class library. To do this we need to add a reference
+	 * 
+	 */
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
