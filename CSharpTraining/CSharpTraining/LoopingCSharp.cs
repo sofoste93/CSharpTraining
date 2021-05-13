b797c8c34d9e1b3205cec5e0a263b70a746f37df
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-13 21:10:50 +0200

    Create LoopingCSharp.cs

---
+++ CSharpTraining/CSharpTraining/LoopingCSharp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+	public class LoopingCSharp
+	{
+		public int ForLoop()
+		{
+			var sum = 0;
+			for (var i = 0; i < 100; i++) // start counting at 0
+			{
+				sum = sum + i; // sum += i;
+				
+			}
+			return sum;
+		}
+	}
+}
