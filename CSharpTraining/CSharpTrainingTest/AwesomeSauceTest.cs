84d814bbf1de6b190c35cdadaeda9447c42b2fef
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-13 18:46:58 +0200

    ..testing

---
+++ CSharpTraining/CSharpTrainingTest/AwesomeSauceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using CSharpTraining;
+
+namespace CSharpTrainingTest
+{
+	[TestClass]
+	public class AwesomeSauceTest
+	{
+		[TestMethod]
+		public void IsSauceAwesomeTest()
+		{
+			var testInstance = new AwesomeSauce();
+			testInstance.Sauces.Add("Tobasco");
+			testInstance.Sauces.Add("Cholula");
+			testInstance.Sauces.Add("Trailer Trash");
+
+			// true expected
+			Assert.IsTrue(testInstance.IsSauceAwesome("Trailer Trash"));
+			// false expected
+			Assert.IsTrue(testInstance.IsSauceAwesome("Ketchup"));
+
+		}
+	}
+}
