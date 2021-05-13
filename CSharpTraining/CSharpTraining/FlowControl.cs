bad230ba061c2f03e4b0a4252ee2b475357d8963
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-13 19:42:45 +0200

    Update FlowControl.cs
    Using logical OR ||
    and logical AND &&

--- CSharpTraining/CSharpTraining/FlowControl.cs
+++ CSharpTraining/CSharpTraining/FlowControl.cs
@@ -8,9 +8,53 @@ namespace CSharpTraining
 {
 	class FlowControl
 	{
+		// Compound logic with logical OR & AND
+		public string PrimOrSecondCompound(string color)
+		{
+			if ((color.ToLower() == "red") || (color.ToLower() == "blue") || (color.ToLower() == "yello"))
+			{
+				return "Primary";
+			} else
+			{
+				return "Secondary";
+
+			}
+
+		}
+
+		// switch-statement
+		public string SecondOrPrimSwitch(string color)
+		{
+			var result = "";
+
+			switch (color.ToLower())
+			{
+				case "red":
+					result = "Primary";
+					// could have more statements here
+					break;
+				case "blue":
+					result = "Primary";
+					// could have more statements here
+					break;
+				case "yellow":
+					result = "Primary";
+					// could have more statements here
+					break;
+
+				default:
+					result = "Secondary";
+					break;
+			}
+			return result;
+	
+		}
+
+		// if-else-statement
 		public string PrimaryOrSecondary(string color)
 		{
 			var result = "";
+
 			if (color.ToLower() == "red")
 			{
 				result = "Primary";
