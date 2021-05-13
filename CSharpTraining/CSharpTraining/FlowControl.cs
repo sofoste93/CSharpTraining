d9976aa0704da66e2266cb625eb3f3e46179581f
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-13 18:50:38 +0200

    create FlowControl.cs

---
+++ CSharpTraining/CSharpTraining/FlowControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+	class FlowControl
+	{
+		public bool IsYourFavoriteColorBlue(string color)
+		{
+			if(color.ToLower() == "blue")
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
