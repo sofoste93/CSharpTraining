10f9dd55380754382146da7b5a126721e86ac8ef
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-13 17:37:30 +0200

    Update SimpleArray.cs

--- CSharpTraining/CSharpTraining/SimpleArray.cs
+++ CSharpTraining/CSharpTraining/SimpleArray.cs
@@ -8,5 +8,19 @@ namespace CSharpTraining
 {
 	class SimpleArray
 	{
+		public string[] GroceryList; // create array
+
+		public SimpleArray()
+		{
+			GroceryList = new string[4] {"Bread", "Milk", "Eggs", "Soda"};
+		}
+
+		public override string ToString()
+		{
+			// return base.ToString();
+			return "There are " + GroceryList.Length + " and they are: "
+				+ GroceryList.ToString();
+		}
+
 	}
 }
