7f9f8b1e74a5f6c6b8de4122b192ccc9060652b8
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-13 18:23:47 +0200

    Update AwesomeSauce.cs

---
+++ CSharpTraining/CSharpTraining/AwesomeSauce.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+	public class AwesomeSauce
+	{
+		public List<string> Sauces { get; set; }
+
+		// : .constructor
+		public AwesomeSauce()
+		{
+			Sauces = new List<string>();
+		}
+		// method: boolean return type
+		public bool IsSauceAwesome(string sauce)
+		{
+			return Sauces.Contains(sauce);
+		}
+	}
+}
