24893eaccc16159a8b76533d55146e2a24318806
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-13 18:56:52 +0200

    Update FlowControl.cs

--- CSharpTraining/CSharpTraining/FlowControl.cs
+++ CSharpTraining/CSharpTraining/FlowControl.cs
@@ -8,6 +8,21 @@ namespace CSharpTraining
 {
 	class FlowControl
 	{
+		public bool IsYourFavoriteColorYellow(string color)
+		{
+			return(color.ToLower() == "yellow");
+		}
+
+		public bool IsYourFavoriteColorRed(string color)
+		{
+			if (color.ToLower() == "red") return true;
+			return false;
+		}
+		public bool IsYourFavoriteColorGreen(string color)
+		{
+			return (color.ToLower() == "green") ? true : false;
+		}
+
 		public bool IsYourFavoriteColorBlue(string color)
 		{
 			if(color.ToLower() == "blue")
